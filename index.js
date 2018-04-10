f0c2cdd44a9c0d28c1e0e937e6cff13364fc6b05
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-04-10 15:33:15 -0700

    create structure, refactor OAuth

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
-app.get('/', (req, res)=> {
+require('./routes/authRoutes')(app);
+
+app.get('/', (req, res) => {
   res.send({hi: 'there'});
-});
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
