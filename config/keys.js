929f3fa2c21fe2bb40f37aa37b75ed33edae0576
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-04-12 18:27:14 -0700

    Fix syntax error

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,8 @@
 // keys.js - what credentials to return
 if (process.env.NODE_ENV === 'production') {
-    // were in production
-    module.exports = require('./prod');
-  else {
-    //return dev keys
-    module.exports = require('./dev');
+  // were in production
+  module.exports = require('./prod');
+} else {
+  //return dev keys
+  module.exports = require('./dev');
   }
-}
