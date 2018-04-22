93c0712d4c88f69c6ee88e56f3bb6e89152173bd
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-04-21 19:59:02 -0700

    Handle POST/PATCH/PUT requests, Handle client side route requests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport');
@@ -9,6 +10,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI)
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -21,10 +24,20 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-})
+if (process.env.NODE_ENV === 'production') {
+  // Express will server up production assets
+  // like main.js or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will server up index.html file if it
+  // doesnt recognize the route
+  const path = require('path');
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
